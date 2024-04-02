96aca18060475d72cd03f24d4d59264b5c5d77fd
evgeniyamuravyeva <me@exante.eu>
2024-04-02 17:01:57 +0300

    change fields filters

--- apis/search_tweets.py
+++ apis/search_tweets.py
@@ -3,7 +3,7 @@ from copy import copy
 from flask_restful import Resource
 from flask import request, jsonify
 
-from src.tweets import OBJECT
+from src.tweets_template import OBJECT
 from utils.auth import auth_checker
 
 
@@ -12,8 +12,16 @@ class SearchTweetsApi(Resource):
     _dummy = OBJECT
 
     @property
-    def data(self):
+    def tweets(self):
         return self._dummy["data"]
+
+    @property
+    def users(self):
+        return self._dummy["includes"]["users"]
+
+    @property
+    def media(self):
+        return self._dummy["includes"]["media"]
 
     # @data.setter
     # def data(self, changes):
@@ -21,20 +29,25 @@ class SearchTweetsApi(Resource):
 
     @auth_checker
     def get(self):
-        tweets_data = self.data
+        tweets_data = self.tweets
+        users = self.users
+        media = self.media
         if query := request.args.get('query', None):
             tweets_data = self.filter_data(tweets_data, query)
         if fields := request.args.get('tweet.fields', None):
-            tweets_data = self.filter_fields(self.data, fields)
-
-        return jsonify({"data": tweets_data})
+            tweets_data = self.filter_fields(tweets_data, fields)
+        if user_fields := request.args.get('user.fields', None):
+            users = self.filter_fields(users, user_fields)
+        if media_fields := request.args.get('media.fields', None):
+            media = self.filter_fields(media, media_fields)
+        return jsonify({"data": tweets_data, "includes": {"users": users, "media": media}})
 
     @staticmethod
     def filter_fields(_object: list, _fields: str) -> list:
         new_data = []
         _fields = _fields.split(',')
-        for tweet in _object:
-            new_tweet = {k: v for k, v in tweet.items() if k in _fields}
+        for i in _object:
+            new_tweet = {k: v for k, v in i.items() if k in _fields}
             new_data.append(new_tweet)
 
         return new_data
